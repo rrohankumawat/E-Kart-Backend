aaa11023ae84950cbcd951908aad2e1d5eae8466
Rohan <rohankumawat39@gmail.com>
2026-02-13 16:26:53 +0530

    add cors

--- E-Kart-Backend/E-Kart-Backend/Program.cs
+++ E-Kart-Backend/E-Kart-Backend/Program.cs
@@ -4,7 +4,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowWeb", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200").
+        AllowAnyMethod().
+        AllowAnyHeader().AllowCredentials();
+    });
+});
+
 builder.Services.AddAppDI();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -18,7 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowWeb");
 app.UseAuthorization();
 
 app.MapControllers();
